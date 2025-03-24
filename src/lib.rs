7f09854252f47d549c6596ea962f623379d657e6
cafalchio <mcafalchio@gmail.com>
2025-03-24 20:52:15 +0000

    fix: removed unused code

--- src/lib.rs
+++ src/lib.rs
@@ -87,31 +87,6 @@ impl TlvObject {
 }
 
 
-
-class TlvVozEricsson:
-    """Tag-Length-Value object for BER encoding for Ericsson Common Charging Output"""
-
-    def __init__(
-        self,
-        tag_number: int,
-        value: bytes,
-        schema: Mapping = None,
-    ):
-        self.length = len(value)
-        if schema is None:  # root
-            tag_mapping = modules.CallDataRecord[tag_number]
-        else:
-            tag_mapping = schema[tag_number]
-        if isinstance(tag_mapping["type"], MAPPING_TYPES):
-            self.name = tag_mapping["tag"]
-            self.value = tag_mapping["name"]
-            self.schema = tag_mapping["type"]
-        else:
-            self.name = tag_mapping["name"]
-            self.value = getattr(tag_mapping["type"](value), "value")
-            self.schema = {}
-
-
 pub struct Data<'a> {
     pub data: &'a [u8],
     pub point: u32,
